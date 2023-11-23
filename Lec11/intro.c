6c5d0e5f45dd192e6c9ec6e85046587c7ce4a35f
Mohammad Israk Chowdhury <israkchowdhury03@gmail.com>
2023-11-23 17:28:40 +0600

    lec 11

---
+++ Lec11/intro.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(){
+    char greetings[] = "Hello World.";
+
+    printf("%s", greetings);
+
+    return 0;
+}
---
+++ Lec11/line_of_text_gets.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    char a[100];
+    printf("Enter your text: ");
+    gets(a);
+    printf("Text is : ");
+    puts(a);
+
+    return 0;
+}
